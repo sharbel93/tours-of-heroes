a061b73573b17667a165af0b80707f162579c017
sharbel chris <sharbel.cosa@gmail.com>
2018-10-12 13:25:15 +0300

    Commit #7 - adding Http

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HeroService} from '../hero.service'; // class
+import {HeroService} from '../hero.service';
+
 
 
 
@@ -10,15 +11,31 @@ import {HeroService} from '../hero.service'; // class
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  constructor(private heroService: HeroService) { }
+    heroes: Hero[];
 
-  ngOnInit() {
-      this.getHeroes();
-  }
-  getHeroes(): void {
-      this.heroService.getHeroes()
-          .subscribe(heroes => this.heroes = heroes);
-  }
+    constructor(private heroService: HeroService) {
+    }
 
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) {
+            return;
+        }
+        this.heroService.addHero({name} as Hero)
+            .subscribe(hero => this.heroes.push(hero));
+    }
+    delete(hero: Hero): void {
+      this.heroes = this.heroes.filter(h => h !== hero);
+      this.heroService.deleteHero(hero).subscribe();
+    }
 }
+
